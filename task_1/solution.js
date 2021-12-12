2e52ddc1b08b94942fb334deb809abe79a128b43
Anna Borodinova <annaborodinova@gmail.com>
2021-12-12 14:57:15 +0300

    Task1 v.1

--- task_1/solution.js
+++ task_1/solution.js
@@ -58,9 +58,14 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
    
+    
     let newSum = oldSum + difference;
 
-    let newSumText = newSum + " ₽";
+    let newSumText;
+
+    newSumText = `${newSum} ₽`
+ 
+
 
     // Конец решения задания №1.3.
 
