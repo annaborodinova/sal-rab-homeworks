94af4f453b4b12249427211e495eb38eccea4ae4
Anna Borodinova <annaborodinova@gmail.com>
2021-12-19 11:49:17 +0300

    Task 2.1 v1 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,9 +5,13 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;// создайте переменную shippingSum
 
-    // если productsSum равно 0,
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {shippingSum = 0;
+        
+    } else {shippingSum = shippingPrice;
+        
+    }// если productsSum равно 0,
     // то shippingSum присвоить значение 0
 
     // если productsSum Больше или равна freeShippingMinSum,
@@ -15,7 +19,7 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+    
     // Конец решения задания №2.1.
 
     return shippingSum;
